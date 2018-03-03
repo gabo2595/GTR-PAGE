db5955516110de6d56f0d8d2238be794b2b9cefc
gabo2595 <gabo2595@gmail.com>
2018-03-02 22:44:43 -0300

    Uploads
    Everleds

--- jsfile.js
+++ jsfile.js
@@ -1,2 +1,76 @@
-function validEmail(email){var re=/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;return re.test(email);}function getFormData(){var elements=document.getElementById("gform").elements;var fields=Object.keys(elements).map(function(k){if(elements[k].name!==undefined){return elements[k].name;}else if(elements[k].length>0){return elements[k].item(0).name;}}).filter(function(item,pos,self){return self.indexOf(item)==pos&&item;});var data={};fields.forEach(function(k){data[k]=elements[k].value;var str="";if(elements[k].type==="checkbox"){str=str+elements[k].checked+", ";data[k]=str.slice(0,-2);}else if(elements[k].length){for(var i=0;i<elements[k].length;i++){if(elements[k].item(i).checked){str=str+elements[k].item(i).value+", ";data[k]=str.slice(0,-2);}}}});document.getElementById('submit-button').style.pointerEvents='none';document.getElementById('submit-button').style.backgroundColor='#cccccc';document.getElementById('submit-button').style.border='1px solid #999999';document.getElementById('submit-button').style.color='#666666';return data;}function handleFormSubmit(event){event.preventDefault();var data=getFormData();if(!validEmail(data.Email)){document.getElementById('email-invalid').style.display='block';console.log("llegue aqui");return false;}else{var url=event.target.action;var xhr=new XMLHttpRequest();xhr.open('POST',url);xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');xhr.onreadystatechange=function(){document.getElementById('gform').reset();document.getElementById('popup1').style.visibility='visible';document.getElementById('popup1').style.opacity='1';document.getElementById('popup1').style.zIndex='99999999';$("#close-btn").click(function(){document.getElementById('popup1').style.visibility='hidden';document.getElementById('popup1').style.opacity='0';document.getElementById('popup1').style.zIndex='-1';});document.getElementById('submit-button').style.pointerEvents='auto';document.getElementById('submit-button').style.backgroundColor='#0961A5';document.getElementById('submit-button').style.border='none';document.getElementById('submit-button').style.color='white';return;};var encoded=Object.keys(data).map(function(k){return encodeURIComponent(k)+'='+encodeURIComponent(data[k])}).join('&')
-xhr.send(encoded);}}function loaded(){var form=document.getElementById('gform');form.addEventListener("submit",handleFormSubmit,false);};document.addEventListener('DOMContentLoaded',loaded,false);
+function validEmail(email) { // see:
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
+// get all data in form and return object
+function getFormData() {
+  var elements = document.getElementById("gform").elements; // all form elements
+  var fields = Object.keys(elements).map(function(k) {
+    if(elements[k].name !== undefined) {
+      return elements[k].name;
+    // special case for Edge's html collection
+    }else if(elements[k].length > 0){
+      return elements[k].item(0).name;
+    }
+  }).filter(function(item, pos, self) {
+    return self.indexOf(item) == pos && item;
+  });
+  var data = {};
+  fields.forEach(function(k){
+    data[k] = elements[k].value;
+    var str = ""; // declare empty string outside of loop to allow
+                  // it to be appended to for each item in the loop
+    if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+      str = str + elements[k].checked + ", "; // take the string and append 
+                                              // the current checked value to 
+                                              // the end of it, along with 
+                                              // a comma and a space
+      data[k] = str.slice(0, -2); // remove the last comma and space 
+                                  // from the  string to make the output 
+                                  // prettier in the spreadsheet
+    }else if(elements[k].length){
+      for(var i = 0; i < elements[k].length; i++){
+        if(elements[k].item(i).checked){
+          str = str + elements[k].item(i).value + ", "; // same as above
+          data[k] = str.slice(0, -2);
+        }
+      }
+    }
+  });
+  //console.log(data);
+  return data;
+}
+
+function handleFormSubmit(event) {  // handles form submit withtout any jquery
+  event.preventDefault();           // we are submitting via xhr below
+  var data = getFormData();         // get the values submitted in the form
+  if( !validEmail(data.email) ) {   // if email is not valid show error
+    document.getElementById('email-invalid').style.display = 'block';
+    //console.log("llegue aqui");
+    return false;
+  } else {
+    var url = event.target.action;  //
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        document.getElementById('gform').style.display = 'none'; // hide form
+        document.getElementById('thankyou_message').style.display = 'block';
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+  }
+}
+function loaded() {
+  //console.log('contact form submission handler loaded successfully');
+  // bind to the submit event of our form
+  //console.log("Aqui tambien");
+  var form = document.getElementById('gform');
+  form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener('DOMContentLoaded', loaded, false);
